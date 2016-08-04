6b7b35c316c2875a913f2f746d4789f11bb517b5
Nadeem Ahmed N <nadeemnajeeb@users.noreply.github.com>
2016-08-04 21:03:33 +0530

    Added cache matrix program
    Week 3 Assignment 2 for JHU Data Science Specialisation - Programming in R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Caching is the process of storing the matrix data in a temporary storage
+# The program's roadmap is as follows:
+# Input matrix
+# Cache the matrix
+# Invoke the function to compute its inverse
+# The cacheSolve() checks if the matrix is in the environment other than itself
+# If found, it simply returns the cache copy without recomupting the result
+# If not found in cache, it computes the inverse, stores it in the cache and returns it
 
-## Write a short comment describing this function
+
+## makeCacheMatrix() caches a given matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Input: A reversible matrix
+  ## Output: The inverse of the reversible matrix
+  inverseMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseMatrix <<- NULL
+  }
+  get <- function() x
+  setInverseMatrix <- function(solve) inverseMatrix <<- solve
+  getInverseMatrix <- function() inverseMatrix
+  list(set = set, get = get,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
-
-## Write a short comment describing this function
+# The cacheSolve() checks if the matrix is in the environment other than itself
+# If found, it simply returns the cache copy without recomupting the result
+# If not found in cache, it computes the inverse, stores it in the cache and returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseMatrix <- x$getInverseMatrix()
+  if(!is.null(inverseMatrix)) {
+    message("Matrix already cached. Fetching cached data")
+    return(inverseMatrix)
+  }
+  temp <- x$get()
+  inverseMatrix <- solve(temp, ...)
+  x$setInverseMatrix(inverseMatrix)
+  inverseMatrix
 }
